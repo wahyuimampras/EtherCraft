21ced747e7450936058ba419d67c7a4cc84a6338
wahyuimampras <wahyu.prasetyo001@binus.ac.id>
2025-10-15 22:05:55 +0700

    upd

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        products: resolve(__dirname, 'products.html'),
-        about: resolve(__dirname, 'about.html'),
-      },
+    build: {
+        rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            products: resolve(__dirname, 'products.html'),
+            about: resolve(__dirname, 'about.html'),
+        },
+        },
     },
-  },
 });
